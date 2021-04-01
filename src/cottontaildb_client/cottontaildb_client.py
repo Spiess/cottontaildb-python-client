4d91c91e714f30b3dbe162c33cecb21b4f4751ee
Florian Spiess <florian.spiess@unibas.ch>
2021-04-01 12:15:44 +0200

    Added more client functionality and refactored class variables into public and private.

--- src/cottontaildb_client/cottontaildb_client.py
+++ src/cottontaildb_client/cottontaildb_client.py
@@ -2,69 +2,75 @@ import grpc
 from google.protobuf.empty_pb2 import Empty
 
 from .cottontail_pb2 import SchemaName, CreateSchemaMessage, DropSchemaMessage, EntityName, ColumnDefinition, \
-    EntityDefinition, CreateEntityMessage, Engine, InsertMessage, ColumnName, Scan, From, Type
+    EntityDefinition, CreateEntityMessage, Engine, InsertMessage, ColumnName, Scan, From, Type, ListSchemaMessage, \
+    ListEntityMessage, EntityDetailsMessage, DropEntityMessage, TruncateEntityMessage, OptimizeEntityMessage
 from .cottontail_pb2_grpc import DDLStub, DMLStub, TXNStub
 
 
 class CottontailDBClient:
     def __init__(self, host, port, with_transaction=False):
-        self.host = host
-        self.port = port
-        self.transaction = with_transaction
-        self.tid = None
+        self._host = host
+        self._port = port
+        self._transaction = with_transaction
+        self._tid = None
 
     def __enter__(self):
-        self.channel = grpc.insecure_channel(f'{self.host}:{self.port}')
-        self.ddl = DDLStub(self.channel)
-        self.dml = DMLStub(self.channel)
-        self.txn = TXNStub(self.channel)
-        if self.transaction:
-            self.tid = self.txn.Begin(Empty())
+        self._channel = grpc.insecure_channel(f'{self._host}:{self._port}')
+        self._ddl = DDLStub(self._channel)
+        self._dml = DMLStub(self._channel)
+        self._txn = TXNStub(self._channel)
+        if self._transaction:
+            self._tid = self._txn.Begin(Empty())
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.transaction:
-            self.txn.Commit(self.tid)
-        self.channel.close()
+        if self._transaction:
+            self._txn.Commit(self._tid)
+        self._channel.close()
 
     # Transactions
 
     def start_transaction(self):
         """Starts a transaction that all further changes will be associated with."""
-        if self.transaction:
+        if self._transaction:
             raise Exception('Transaction already running!')
-        self.transaction = True
-        self.tid = self.txn.Begin(Empty())
+        self._transaction = True
+        self._tid = self._txn.Begin(Empty())
 
     def commit_transaction(self):
         """Commits the current transaction."""
-        if not self.transaction:
+        if not self._transaction:
             raise Exception('No transaction running!')
-        self.txn.Commit(self.tid)
-        self.tid = None
-        self.transaction = False
+        self._txn.Commit(self._tid)
+        self._tid = None
+        self._transaction = False
 
     def abort_transaction(self):
         """Aborts the current transaction."""
-        if not self.transaction:
+        if not self._transaction:
             raise Exception('No transaction running!')
-        self.txn.Rollback(self.tid)
-        self.tid = None
-        self.transaction = False
+        self._txn.Rollback(self._tid)
+        self._tid = None
+        self._transaction = False
+
+    def list_transactions(self):
+        """Lists all transactions."""
+        responses = [response for response in self._txn.ListTransactions(Empty())]
+        return [r for response in responses for r in self._parse_query_response(response)]
 
     # Data definition
 
-    def create_schema(self, name):
+    def create_schema(self, schema):
         """Creates a new schema with the given name."""
-        schema_name = SchemaName(name=name)
-        self.ddl.CreateSchema(CreateSchemaMessage(txId=self.tid, schema=schema_name))
+        schema_name = SchemaName(name=schema)
+        self._ddl.CreateSchema(CreateSchemaMessage(txId=self._tid, schema=schema_name))
 
-    def drop_schema(self, name):
+    def drop_schema(self, schema):
         """Drops the schema with the given name."""
-        schema_name = SchemaName(name=name)
-        self.ddl.DropSchema(DropSchemaMessage(schema=schema_name))
+        schema_name = SchemaName(name=schema)
+        self._ddl.DropSchema(DropSchemaMessage(txId=self._tid, schema=schema_name))
 
-    def create_entity(self, schema, name, columns):
+    def create_entity(self, schema, entity, columns):
         """
         Creates an entity in the given schema with the defined columns.
 
@@ -72,13 +78,86 @@ class CottontailDBClient:
         columns = [column_def('id', Type.STRING, nullable=False)]
 
         @param schema: name of the entity's schema
-        @param name: entity name
+        @param entity: entity name
         @param columns: list of ColumnDefinition objects defining the entity's columns
         """
         schema_name = SchemaName(name=schema)
-        entity_name = EntityName(schema=schema_name, name=name)
-        entity = EntityDefinition(entity=entity_name, columns=columns)
-        self.ddl.CreateEntity(CreateEntityMessage(txId=self.tid, definition=entity))
+        entity_name = EntityName(schema=schema_name, name=entity)
+        entity_def = EntityDefinition(entity=entity_name, columns=columns)
+        self._ddl.CreateEntity(CreateEntityMessage(txId=self._tid, definition=entity_def))
+
+    def drop_entity(self, schema, entity):
+        """Drops the given entity from the given schema."""
+        schema_name = SchemaName(name=schema)
+        entity_name = EntityName(schema=schema_name, name=entity)
+        self._ddl.DropEntity(DropEntityMessage(txId=self._tid, entity=entity_name))
+
+    def truncate_entity(self, schema, entity):
+        """Truncates the specified entity."""
+        schema_name = SchemaName(name=schema)
+        entity_name = EntityName(schema=schema_name, name=entity)
+        self._ddl.DropEntity(TruncateEntityMessage(txId=self._tid, entity=entity_name))
+
+    def optimize_entity(self, schema, entity):
+        """Optimizes the specified entity."""
+        schema_name = SchemaName(name=schema)
+        entity_name = EntityName(schema=schema_name, name=entity)
+        self._ddl.DropEntity(OptimizeEntityMessage(txId=self._tid, entity=entity_name))
+
+    # TODO: Indexes
+
+    def list_schemas(self):
+        """Lists all schemas in the database."""
+        responses = [response for response in self._ddl.ListSchemas(ListSchemaMessage(txId=self._tid))]
+        tuples = [t.data[0].stringData for response in responses for t in response.tuples]
+        return tuples
+
+    def list_entities(self, schema):
+        """
+        Lists all entities of the specified schema
+
+        @param schema: schema of which to list entities
+        @return: list of entity names
+        """
+        schema_name = SchemaName(name=schema)
+        responses = [response for response in
+                     self._ddl.ListEntities(ListEntityMessage(txId=self._tid, schema=schema_name))]
+        tuples = [t.data[0].stringData for response in responses for t in response.tuples]
+        return tuples
+
+    def get_entity_details(self, schema, entity):
+        """
+        Retrieves details about an entity.
+
+        @param schema: the entity's schema
+        @param entity: entity name
+        @return: dictionary containing entity details
+        """
+        schema_name = SchemaName(name=schema)
+        entity_name = EntityName(schema=schema_name, name=entity)
+        response = self._ddl.EntityDetails(EntityDetailsMessage(txId=self._tid, entity=entity_name))
+        entity_data = response.tuples[0]
+        data_names = [c.name for c in response.columns]
+        name_index = data_names.index('dbo')
+        class_index = data_names.index('class')
+        type_index = data_names.index('type')
+        size_index = data_names.index('l_size')
+        nullable_index = data_names.index('nullable')
+        entity_details = {
+            'name': entity_data.data[name_index].stringData,
+            'rows': entity_data.data[data_names.index('rows')].intData,
+            'columns': [
+                {
+                    'name': c.data[name_index].stringData,
+                    'type': c.data[type_index].stringData,
+                    'size': c.data[size_index].intData,
+                    'nullable': c.data[nullable_index].booleanData
+                } for c in response.tuples if c.data[class_index].stringData == 'COLUMN'
+            ]
+            # TODO: Index data
+        }
+
+        return entity_details
 
     # Data management
 
@@ -92,7 +171,7 @@ class CottontailDBClient:
         @return: query response message
         """
         message = self._insert_helper(schema, entity, values)
-        return self.dml.Insert(message)
+        return self._dml.Insert(message)
 
     def insert_batch(self, insert_iterator):
         """
@@ -106,8 +185,19 @@ class CottontailDBClient:
             for schema, entity, values in insert_iterator:
                 yield self._insert_helper(schema, entity, values)
 
-        for _ in self.dml.InsertBatch(insert_stream()):
+        for _ in self._dml.InsertBatch(insert_stream()):
             continue
+
+    @staticmethod
+    def _parse_query_response(response):
+        data_names = [c.name for c in response.columns]
+        return [
+            {
+                key: value for key, value in zip(data_names, item.data)
+            } for item in response.tuples
+        ]
+
+    # TODO: Update, Delete
 
     def _insert_helper(self, schema, entity, values):
         schema_name = SchemaName(name=schema)
@@ -115,7 +205,7 @@ class CottontailDBClient:
         from_arg = {'from': From(scan=Scan(entity=entity_name))}
         elements = [InsertMessage.InsertElement(column=ColumnName(name=column_name), value=value)
                     for column_name, value in values.items()]
-        return InsertMessage(txId=self.tid, **from_arg, inserts=elements)
+        return InsertMessage(txId=self._tid, **from_arg, inserts=elements)
 
 
 def column_def(name: str, type_: Type, length: int = None, primary: bool = None, nullable: bool = None,
