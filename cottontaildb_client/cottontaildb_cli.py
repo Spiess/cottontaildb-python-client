1180c672d208613146f86de4258261b5dc8bc73c
Florian Spiess <florian.spiess@unibas.ch>
2021-05-01 18:40:57 +0200

    Added system and entity detail functionality to interactive CLI.

--- cottontaildb_client/cottontaildb_cli.py
+++ cottontaildb_client/cottontaildb_cli.py
@@ -59,16 +59,29 @@ def get_command_parser():
     parser_schema_list.add_argument('schema_name', help='Name of schema to list entities of.')
 
     # Entity
-    parser_schema = subparsers.add_parser('entity', help='Entity related commands.')
-    subparsers_schema = parser_schema.add_subparsers(dest='subcommand')
+    parser_entity = subparsers.add_parser('entity', help='Entity related commands.')
+    subparsers_entity = parser_entity.add_subparsers(dest='subcommand')
     # Entity create
-    parser_schema_create = subparsers_schema.add_parser('create', help='Creates a new entity in the database.')
-    parser_schema_create.add_argument('schema_name', help='Name of schema to create entity in.')
-    parser_schema_create.add_argument('entity_name', help='Name of entity to create.')
+    # parser_entity_create = subparsers_schema.add_parser('create', help='Creates a new entity in the database.')
+    # parser_entity_create.add_argument('schema_name', help='Name of schema to create entity in.')
+    # parser_entity_create.add_argument('entity_name', help='Name of entity to create.')
     # Entity drop
-    parser_schema_create = subparsers_schema.add_parser('drop', help='Drops the given entity from the database.')
-    parser_schema_create.add_argument('schema_name', help='Name of schema to create entity in.')
-    parser_schema_create.add_argument('entity_name', help='Name of entity to create.')
+    parser_entity_drop = subparsers_entity.add_parser('drop', help='Drops the given entity from the database.')
+    parser_entity_drop.add_argument('schema_name', help='Name of schema to drop entity from.')
+    parser_entity_drop.add_argument('entity_name', help='Name of entity to drop.')
+    # Entity about
+    parser_entity_about = subparsers_entity.add_parser('about', help='Gives an overview of the entity and its columns.')
+    parser_entity_about.add_argument('schema_name', help='Name of schema containing entity.')
+    parser_entity_about.add_argument('entity_name', help='Name of entity to show details of.')
+
+    # System
+    parser_system = subparsers.add_parser('system', help='System related commands.')
+    subparsers_system = parser_system.add_subparsers(dest='subcommand')
+    # Transactions
+    subparsers_system.add_parser('transactions',
+                                 help='Lists all ongoing transaction in the current Cottontail DB instance.')
+    # Locks
+    subparsers_system.add_parser('locks', help='Lists all locks in the current Cottontail DB instance.')
 
     # Stop / quit
     subparsers.add_parser('stop', help='Stop client and exit.')
@@ -86,6 +99,8 @@ def process_command(client, args):
         schema(client, args)
     elif command == 'entity':
         entity(client, args)
+    elif command == 'system':
+        system(client, args)
 
     return True
 
@@ -109,5 +124,18 @@ def schema(client, args):
 def entity(client, args):
     command = args.subcommand
     if command == 'drop':
-        response = client.drop_entity(args.schema, args.entity)
+        response = client.drop_entity(args.schema_name, args.entity_name)
+        print(response)
+    elif command == 'about':
+        response = client.get_entity_details(args.schema_name, args.entity_name)
+        print(response)
+
+
+def system(client, args):
+    command = args.subcommand
+    if command == 'transactions':
+        response = client.list_transactions()
+        print(response)
+    elif command == 'locks':
+        response = client.list_locks()
         print(response)
